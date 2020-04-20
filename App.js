193fc691256e293a627751864fcee6129f3029bc
Alvaro <6666alvaro666@gmail.com>
2020-04-20 12:58:04 -0500

    the reception of messeges is ready

--- App.js
+++ App.js
@@ -14,6 +14,34 @@ app.listen(app.get('port'),()=>{
     console.log("server in port"+process.env.PORT);
 });
 
-app.get('/',(req, res)=>{
-    res.send('servidor online')
+app.get('/webhook',(req,res)=>{
+    const mode = req.query['hub.mode'];
+    const challenge = req.query['hub.challenge'];
+    const token = req.query['hub.verify_token'];
+
+    if(mode && token){
+        if(mode == 'suscribe'&& token == process.env.VERIFYTOKEN);
+        console.log("webhook listo");
+        res.status(200).send(challenge);
+    }else{
+        res.status(403)   
+    }
+})
+
+app.post('/webhook',(req,res)=>{
+    const body = req.body;
+    if(body.object === "page"){
+        res.status(200).send("EVENT_RECEIVED");
+        body.entry.forEach(function(entry){
+            let webhookEvent = entry.messaging[0];
+            console.log(webhookEvent);
+        });
+    }else{
+        send.status(404);
+    }
 });
+
+app.get('/',(req, res)=>{
+    res.send('servidor')
+});
+
